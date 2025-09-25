c82e42c67f096228464c9715149e959468f77cf1
Orekhov Michael <milkorehov@gmail.com>
2025-09-25 13:43:22 +0300

    dataset update to 384 samples per class

--- camera.py
+++ camera.py
@@ -3,10 +3,12 @@ import mediapipe as mp
 import time
 import numpy as np
 import os
+import time
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
+VERBOSE_TIME = True
 SAVE_DIR = 'dataset'
 os.makedirs(SAVE_DIR, exist_ok=True)
 
@@ -28,15 +30,15 @@ with open('nums.txt', 'r') as file:
 label = 1
 recording = False
 
+print(f'camera fps: {cap.get(cv2.CAP_PROP_FPS)}')
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = hands.process(rgb_frame)
-
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
@@ -57,41 +59,51 @@ while cap.isOpened():
             np.save(os.path.join(f'{SAVE_DIR}/class_1/class{1}_sample_{str(sample_true_id)}.npy'),
                     np.array(buffer, dtype=np.float32))
             sample_true_id += 1
+            
+            print(f'Запись прекратилась. {sample_true_id}')
+
+            if VERBOSE_TIME:
+                end = time.time()
+                print(f'Время на 30 кадров: {end - start}')
         else:
             np.save(os.path.join(f'{SAVE_DIR}/class_0/class_{0}_sample_{str(sample_false_id)}.npy'),
                     np.array(buffer, dtype=np.float32))
             sample_false_id += 1
+        
+            print(f'Запись прекратилась. {sample_false_id}')
 
+            if VERBOSE_TIME:
+                end = time.time()
+                print(f'Время на 30 кадров: {end - start}')
+        
         recording = False
         buffer = []
 
-        print(f'Запись прекратилась')
 
     cv2.imshow('Frame', frame)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
         label = 1
-        # for i in range(2, 0, -1):
-        #     print(f'Запись через: {i}')
-        #     time.sleep(0.9)
-
         print(f'Запись пошла')
         recording = True
+        
+        if VERBOSE_TIME:
+            start = time.time()
 
     elif key == ord('e'):
         label = 0
-        # for i in range(2, 0, -1):
-        #    print(f'Запись через: {i}')
-        #    time.sleep(0.9)
-
         print(f'Запись пошла')
         recording = True
+        
+        if VERBOSE_TIME:
+            start = time.time()
+
 
     elif key == 27:
         with open('nums.txt', 'w') as file:
             file.writelines(f'{sample_true_id} {sample_false_id}')
         break
-
+    
 cap.release()
 cv2.destroyAllWindows()
